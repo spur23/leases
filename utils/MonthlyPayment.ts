9f9c3637ee0a515876e1bbb3d1abe028c852714f
spur23 <63126196+spur23@users.noreply.github.com>
2021-02-22 09:31:34 -0600

    Create MonthlyPayment.ts

---
+++ utils/MonthlyPayment.ts
@@ -0,0 +1,30 @@
+import { PaymentFrequency } from '../enums';
+import { addMonth } from './addMonth';
+
+export default (
+  y: number,
+  startDate: string,
+  payment: number,
+  frequency: PaymentFrequency
+): { nextMonth: Date; monthlyPayment: number } => {
+  let nextMonth = addMonth(new Date(startDate), y);
+  let monthlyPayment = payment;
+
+  if (y === 0) {
+    nextMonth = new Date(startDate);
+  } else if (frequency === PaymentFrequency.Annual) {
+    if (y % 12 !== 0) {
+      monthlyPayment = 0;
+    }
+  } else if (frequency === PaymentFrequency.SemiAnnual) {
+    if (y % 6 !== 0) {
+      monthlyPayment = 0;
+    }
+  } else if (frequency === PaymentFrequency.Quarterly) {
+    if (y % 3 !== 0) {
+      monthlyPayment = 0;
+    }
+  }
+
+  return { nextMonth, monthlyPayment };
+};
