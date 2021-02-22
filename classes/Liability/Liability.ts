00824e0665ef1d14bd740d63339276aea55c7667
spur23 <63126196+spur23@users.noreply.github.com>
2021-02-22 09:45:38 -0600

    refactor schedule calcuation

--- classes/Liability/Liability.ts
+++ classes/Liability/Liability.ts
@@ -1,6 +1,6 @@
 import { LiabilitySchedule } from '../../interfaces';
 import { PaymentStream } from '../../interfaces';
-import { roundNumber } from '../../utils';
+import { calculateLiability, roundNumber } from '../../utils';
 import { LiabilityMonthly } from './LiabilityMonthly';
 
 export class Liability {
@@ -28,78 +28,14 @@ export class Liability {
   }
 
   calculateMonthlySchedule(): LiabilityMonthly[] {
-    let result = [];
-
-    for (let i = 0; i < this.paymentStream.length; i++) {
-      const date = new Date(this.paymentStream[i].month);
-      const payment = this.paymentStream[i].payment;
-
-      if (i === 0) {
-        const month = new LiabilityMonthly(
-          date,
-          payment,
-          this.startingBalance,
-          this.interestRate,
-          0,
-          this.prepaid
-        );
-
-        result.push(month);
-      } else {
-        let month;
-        const { interestExpense, endingBalance } = result[
-          i - 1
-        ].getMonthlyData();
-
-        if (this.prepaid) {
-          month = new LiabilityMonthly(
-            date,
-            payment,
-            endingBalance,
-            this.interestRate,
-            interestExpense,
-            this.prepaid
-          );
-        } else {
-          month = new LiabilityMonthly(
-            date,
-            payment,
-            endingBalance,
-            this.interestRate,
-            0,
-            this.prepaid
-          );
-        }
-
-        result.push(month);
-      }
-    }
-
-    result.sort(
-      (a, b) => new Date(a.date).valueOf() - new Date(b.date).valueOf()
+    const monthlySchedule = calculateLiability(
+      this.paymentStream,
+      this.startingBalance,
+      this.interestRate,
+      this.prepaid
     );
 
-    let stBalance = 0;
-    let ltBalance = 0;
-
-    for (let i = 0; i < result.length; i++) {
-      if (i < result.length - 12) {
-        for (let y = 0; y < 12; y++) {
-          stBalance += result[y + i].principal;
-        }
-        ltBalance = result[i].endingBalance - stBalance;
-        result[i].shortTermBalance = roundNumber(stBalance, 2);
-        result[i].longTermBalance = roundNumber(ltBalance, 2);
-      } else {
-        result[i].shortTermBalance = roundNumber(result[i].endingBalance, 2);
-        result[i].longTermBalance = 0;
-      }
-
-      stBalance = 0;
-      ltBalance = 0;
-    }
-
-    return result;
+    return monthlySchedule;
   }
 
   getLiabilityData(): LiabilitySchedule[] {
