f49daab472bdea34d7059adc37e6496b55f81a6d
spur23 <spurius1987@gmail.com>
2021-02-22 17:58:04 -0500

    update

--- index.ts
+++ index.ts
@@ -3,35 +3,82 @@ import { PaymentFrequency } from './enums';
 import { Payments } from './classes/Payments/Payments';
 import { Lease } from './classes/Lease';
 import { LeaseClassification } from './enums/LeaseClassification';
+import { data } from './data';
 
-try {
-  const discountRate = 4.6;
-  const name = 'vehicles';
-  const description = 'Vehicles leased in Mexico';
-  const paymentStream1 = [
-    new Payment({
-      payment: 100,
-      frequency: PaymentFrequency.Annual,
-      startDate: '5/1/2020',
-      endDate: '12/31/2020'
-    })
-  ];
-
-  const payments = new Payments(paymentStream1);
-
-  const test = new Lease(
-    name,
+const createLeaseFromJSON = (json) => {
+  const {
+    lease,
+    prepaid,
     description,
-    LeaseClassification.FINANCE,
-    discountRate,
+    classification,
+    interestRate,
+    startDate,
+    endDate,
     payments,
-    true
+    asset,
+    liability
+  } = json;
+
+  const paymentArray = payments.map(
+    (el) =>
+      new Payment({
+        payment: el.payment,
+        frequency: el.frequency,
+        startDate: new Date(el.startDate).toLocaleDateString(),
+        endDate: new Date(el.endDate).toLocaleDateString()
+      })
   );
 
-  // console.log(payments.paymentStream());
-  // const test = new AssetFinance('1/1/2020', 1000, 12);
-  // test.getLeaseInformation();
-  console.log(test.getLeaseInformation());
+  const paymentObjects = new Payments(paymentArray);
+  const leaseClassification =
+    classification === 'operating'
+      ? LeaseClassification.OPERATING
+      : LeaseClassification.FINANCE;
+
+  const newLease = new Lease(
+    lease,
+    description,
+    leaseClassification,
+    interestRate,
+    paymentObjects,
+    prepaid,
+    true,
+    liability,
+    asset
+  );
+
+  console.log(newLease.getLeaseInformation());
+};
+
+try {
+  createLeaseFromJSON(data);
+  // const discountRate = 4.6;
+  // const name = 'vehicles';
+  // const description = 'Vehicles leased in Mexico';
+  // const paymentStream1 = [
+  //   new Payment({
+  //     payment: 100,
+  //     frequency: PaymentFrequency.Monthly,
+  //     startDate: '1/1/2020',
+  //     endDate: '4/31/2020'
+  //   })
+  // ];
+
+  // const payments = new Payments(paymentStream1);
+
+  // const test = new Lease(
+  //   name,
+  //   description,
+  //   LeaseClassification.OPERATING,
+  //   discountRate,
+  //   payments,
+  //   false
+  // );
+
+  // // console.log(payments.paymentStream());
+  // // const test = new AssetFinance('1/1/2020', 1000, 12);
+  // // test.getLeaseInformation();
+  // console.log(test.getLeaseInformation());
   // const liability = new Liability('1/1/2020', 100, 0.046, 2400, 24);
   // console.log(liability);
 } catch (err) {
