8b6ab9d32b3c76c3dd215aa45d5b9465f24de16d
spur23 <63126196+spur23@users.noreply.github.com>
2021-02-22 08:45:39 -0600

    update

--- enums/Prepaid.ts
+++
@@ -1,4 +0,0 @@
-export enum Prepaid {
-  YES = 'yes',
-  NO = 'no'
-}
--- enums/index.ts
+++ enums/index.ts
@@ -1,5 +1,4 @@
 import { LeaseClassification } from './LeaseClassification';
 import { PaymentFrequency } from './PaymentFrequency';
-import { Prepaid } from './Prepaid';
 
-export { LeaseClassification, PaymentFrequency, Prepaid };
+export { LeaseClassification, PaymentFrequency };
