cf520819bbd2a0ed66bf0a6c1f3118c1fbaee611
spur23 <63126196+spur23@users.noreply.github.com>
2021-02-22 09:46:12 -0600

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,13 @@
 import { roundNumber } from './roundNumber';
 import { addMonth } from './addMonth';
 import paymentStreamCalc from './paymentStreamCalc';
+import monthlyCalculation from './monthlyPayment';
+import calculateLiability from './calculateLiability';
 
-export { roundNumber, addMonth, paymentStreamCalc };
+export {
+  roundNumber,
+  addMonth,
+  paymentStreamCalc,
+  monthlyCalculation,
+  calculateLiability
+};
