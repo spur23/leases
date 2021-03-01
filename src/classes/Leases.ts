c3027f4fcd0fbfca97c2cb23d96e31f9095ed657
spur23 <spurius1987@gmail.com>
2021-03-01 15:52:11 -0500

    add

--- src/classes/Leases.ts
+++ src/classes/Leases.ts
@@ -1,18 +1,22 @@
-// import { Lease } from './Lease';
-// export class Leases {
-//   private leases: Lease[];
-//   private leaseQty: number;
+import { Lease } from './Lease';
 
-//   setProperties() {
-//     this.leaseQty = this.leases.length;
-//   }
+export class Leases {
+  leases: Lease[];
+  leaseQty: number;
+  constructor() {
+    this.leases = [];
+    this.leaseQty = 0;
+  }
+  setProperties() {
+    this.leaseQty = this.leases.length;
+  }
 
-//   addLease(lease: Lease) {
-//     this.leases.push(lease);
-//     this.setProperties();
-//   }
+  addLease(lease: Lease) {
+    this.leases.push(lease);
+    this.setProperties();
+  }
 
-//   getLease(leaseName: string) {
-//     return this.leases.find((lease: Lease) => lease.name === leaseName);
-//   }
-// }
+  getLease(leaseName: string): Lease {
+    return this.leases.find((lease: Lease) => lease.name === leaseName);
+  }
+}
