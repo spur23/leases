d7ad68a093a8293d58a699cacfb824626bface9b
spur23 <spurius1987@gmail.com>
2021-03-01 11:39:08 -0500

    updates

--- build/enums/LeaseClassification.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LeaseClassification = void 0;
-var LeaseClassification;
-(function (LeaseClassification) {
-    LeaseClassification["FINANCE"] = "finance";
-    LeaseClassification["OPERATING"] = "operating";
-})(LeaseClassification = exports.LeaseClassification || (exports.LeaseClassification = {}));
--- build/utils/roundNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roundNumber = void 0;
-var roundNumber = function (number, decimals) {
-    return Number(number.toFixed(decimals));
-};
-exports.roundNumber = roundNumber;
