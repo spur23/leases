25f828e867545c6f7fd1f12dd95c35baa50a8048
spur23 <spurius1987@gmail.com>
2021-03-09 08:41:01 -0500

    update

--- src/utils/calculateLiability.ts
+++ src/utils/calculateLiability.ts
@@ -1,5 +1,6 @@
 import { roundNumber } from '.';
 import { LiabilityMonthly } from '../classes/Liability/LiabilityMonthly';
+import { PaymentFrequency } from '../enums';
 import { PaymentStream } from '../interfaces';
 /**
  *Calculates liability monthly schedule.
@@ -15,44 +16,165 @@ const calculateLiability = (
 
   for (let i = 0; i < payments.length; i++) {
     const date = new Date(payments[i].month);
-    const payment = payments[i].payment;
+    const { frequency, payment } = payments[i];
+
+    let annualPayments: number;
+
+    if (frequency === PaymentFrequency.Monthly) {
+      annualPayments = 12;
+    } else if (frequency === PaymentFrequency.Quarterly) {
+      annualPayments = 4;
+    } else if (frequency === PaymentFrequency.SemiAnnual) {
+      annualPayments = 2;
+    } else if (frequency === PaymentFrequency.Annual) {
+      annualPayments = 1;
+    }
 
     if (i === 0) {
-      const month = new LiabilityMonthly(
-        date,
-        payment,
-        startingBalance,
-        interestRate,
-        0,
-        prepaid
-      );
+      if (prepaid) {
+        const interestExpense =
+          (startingBalance - payment) * (interestRate / annualPayments);
+        const principal = payment;
+        const interestPayment = 0;
+        const endingBalance =
+          startingBalance + interestExpense - principal - interestPayment;
 
-      result.push(month);
+        const month = new LiabilityMonthly(
+          date,
+          payment,
+          roundNumber(principal, 2),
+          roundNumber(startingBalance, 2),
+          interestPayment,
+          roundNumber(interestExpense, 2),
+          roundNumber(interestPayment, 2),
+          roundNumber(endingBalance, 2),
+          prepaid
+        );
+
+        result.push(month);
+      } else {
+        const interestExpense =
+          startingBalance * (interestRate / annualPayments);
+        const principal = payment;
+        const interestPayment = 0;
+        const endingBalance =
+          startingBalance + interestExpense - principal - interestPayment;
+
+        console.log(
+          startingBalance,
+          principal,
+          interestPayment,
+          interestExpense,
+          interestPayment,
+          endingBalance
+        );
+        const month = new LiabilityMonthly(
+          date,
+          payment,
+          roundNumber(principal, 2),
+          roundNumber(startingBalance, 2),
+          interestPayment,
+          roundNumber(interestExpense, 2),
+          roundNumber(interestPayment, 2),
+          roundNumber(endingBalance, 2),
+          prepaid
+        );
+
+        result.push(month);
+      }
+
+      // const month = new LiabilityMonthly(
+      //   date,
+      //   payment,
+      //   startingBalance,
+      //   interestRate,
+      //   0,
+      //   prepaid
+      // );
+
+      // result.push(month);
     } else {
-      let month: LiabilityMonthly;
+      // let month: LiabilityMonthly;
+
       const { interestExpense, endingBalance } = result[i - 1].getMonthlyData();
 
       if (prepaid) {
-        month = new LiabilityMonthly(
+        const { interestExpense, endingBalance } = result[
+          i - 1
+        ].getMonthlyData();
+
+        let currentMonthInterestExpense =
+          (endingBalance - payment) * (interestRate / annualPayments);
+
+        const principal = payment - interestExpense;
+
+        const interestPayment = interestExpense;
+
+        const currentMonthEndingBalance =
+          endingBalance +
+          currentMonthInterestExpense -
+          principal -
+          interestPayment;
+
+        if (i === payments.length - 1) {
+          currentMonthInterestExpense = 0;
+        }
+
+        const month = new LiabilityMonthly(
           date,
           payment,
-          endingBalance,
-          interestRate,
-          interestExpense,
-          prepaid
-        );
-      } else {
-        month = new LiabilityMonthly(
-          date,
-          payment,
-          endingBalance,
-          interestRate,
+          roundNumber(principal, 2),
+          roundNumber(endingBalance, 2),
           0,
+          roundNumber(currentMonthInterestExpense, 2),
+          roundNumber(interestPayment, 2),
+          roundNumber(currentMonthEndingBalance, 2),
           prepaid
         );
+
+        result.push(month);
+      } else {
+        const { interestExpense, endingBalance } = result[
+          i - 1
+        ].getMonthlyData();
+
+        const currentMonthInterestExpense =
+          startingBalance * (interestRate / annualPayments);
+
+        const principal = payment - currentMonthInterestExpense;
+
+        const interestPayment = 0;
+
+        const currentMonthEndingBalance =
+          endingBalance +
+          currentMonthInterestExpense -
+          principal -
+          interestPayment;
+
+        const month = new LiabilityMonthly(
+          date,
+          payment,
+          roundNumber(principal, 2),
+          roundNumber(endingBalance, 2),
+          0,
+          roundNumber(currentMonthInterestExpense, 2),
+          roundNumber(interestPayment, 2),
+          roundNumber(currentMonthEndingBalance, 2),
+          prepaid
+        );
+
+        result.push(month);
+        // month = new LiabilityMonthly(
+        //   date,
+        //   payment,
+        //   endingBalance,
+        //   interestRate,
+        //   0,
+        //   prepaid
+        // );
       }
 
-      result.push(month);
+      // result.push(month);
     }
   }
 
